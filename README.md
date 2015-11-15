a453e1c08b8f039e1357cce428185ab164f1c34b
Justin Pradier <chromecide@chromecide.com>
2015-11-15 11:20:14 +1100

    updated readme, added number-inc and number-dec

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # node-red-contrib-primitives
 Simple node-red nodes for working with primitive values
+
+## Arrays
+
+Nodes for working with arrays
+
+## Booleans
+
+Nodes for working with boolean values
+
+## Numbers
+
+Nodes for working with Numbers
+
+### number-inc
+
+Increments the value of a property
+
+### number-dec
+
+Decrements the value of a property
+
+## Strings
+
+Nodes for working with Strings
+
+### string-split
+
+Splits the string value of a property on the configured character.
+
---
+++ numbers/decrement.js
@@ -0,0 +1,60 @@
+module.exports = function(RED) {
+    function NumberDecrementNode(config) {
+        
+        RED.nodes.createNode(this,config);
+        var node = this;
+
+        node.source_object = config.source_object||"msg";
+        node.source_property = config.source_property||"payload";
+        node.source_property_parts = node.source_property.split(".");
+
+        node.target_object = config.source_object||"msg";
+        node.target_property = config.source_property||"payload";
+        node.target_property_parts = node.target_property.split(".");
+
+        node.decrement_by = config.decrement_by*1 || 1;
+        
+        this.on('input', function(msg) {
+            var sourceObj = msg;
+            if(node.source_object=="context"){
+                sourceObj = msg;
+            }else if(node.source_object=="context.global"){
+                sourceObj = context.global;
+            }
+
+            var propVal = node.source_property_parts.reduce(function(obj, i){
+                return obj[i];
+            }, sourceObj);
+
+            if(isNaN(propVal)){
+                node.send(msg);
+                return;
+            }else{
+                propVal = propVal*1;
+            }
+
+            var obj = sourceObj;
+
+            for(var i=0;i<node.target_property_parts.length;i++){
+                var propName = node.target_property_parts[i];
+                if((typeof obj[propName])=="undefined"){
+                    if(i<node.target_property_parts.length-1){
+                        obj[propName] = {};
+                    }else{
+                        obj[propName] = 0;
+                    }
+                }
+
+                if(i<node.target_property_parts.length-1){
+                    obj = obj[propName];
+                }else{
+                    obj[propName] = propVal-node.decrement_by;
+                }
+            }
+
+            node.send(msg);
+        });
+    }
+
+    RED.nodes.registerType("number-dec", NumberDecrementNode);
+};
